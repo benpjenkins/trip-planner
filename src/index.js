8ccc85072f59ca55d6c9b806d5e5afa64a9c0dce
Ben Jenkins <ben.jenkins@icloud.com>
2019-06-24 10:41:25 -0500

    add map box

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYmVuamVua2luc2RldiIsImEiOiJjanhham5hbDUxNzdjM3pxYmpwb21jZGw5In0.rwNJu7SgFpQL3RBdJpJSnw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-87.6354, 41.8885],
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
+
 console.log('index file is working');
